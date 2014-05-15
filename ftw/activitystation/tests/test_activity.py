5a4e23dc501f4fe2af08769f117c0b619a4b7a7f
David Erni <david.erni@4teamwork.ch>
2014-05-15 19:13:08 +0200

    Improve test for activity station event trigger.

--- ftw/activitystation/tests/test_activity.py
+++ ftw/activitystation/tests/test_activity.py
@@ -2,7 +2,8 @@ from ftw.activitystation import sender
 from ftw.activitystation.tests.base import FunctionalTestCase
 from ftw.builder import Builder
 from ftw.builder import create
-from plone import api
+from plone.app.testing.interfaces import TEST_USER_ID
+from plone.uuid.interfaces import IUUID
 
 
 class TestSender(object):
@@ -13,6 +14,12 @@ class TestSender(object):
     def post(self, event):
         self.queue.append(event)
 
+    def __len__(self):
+        return len(self.queue)
+
+    def pop(self):
+        return self.queue.pop()
+
 
 class TestActivity(FunctionalTestCase):
 
@@ -21,8 +28,19 @@ class TestActivity(FunctionalTestCase):
         sender.SENDER = self.sender = TestSender()
 
     def test_content_rule_triggers_activity_station_events(self):
-        # create(Builder('folder'))
-        parent = api.content.create(type='Folder', title='hmm',
-                                    container=self.portal)
+        folder = create(Builder('folder').titled('Testfolder'))
 
-        self.assertEqual(1, len(self.sender.queue))
+        self.assertEqual(1, len(self.sender))
+        activity = self.sender.pop()
+
+        self.assertIn('payload', activity)
+        payload = activity['payload']
+        self.assertEqual('Testfolder', payload['title'])
+        self.assertEqual(IUUID(folder), payload['uuid'])
+
+        self.assertEqual('test_event', activity['kind'])
+        self.assertEqual(TEST_USER_ID, activity['actor'])
+        self.assertEqual(folder.absolute_url(), activity['url'])
+        self.assertEqual("/".join(folder.getPhysicalPath()),
+                         activity['path'])
+        self.assertIn('message', activity)
